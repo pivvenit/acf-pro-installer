c17e0d5294d92d22734f1eb9b27ef8052e7d42eb
Sander Klock <680327+Qrious@users.noreply.github.com>
2020-03-04 19:36:37 +0100

    Add support for new download URL

--- src/Download/DownloadMatcher.php
+++ src/Download/DownloadMatcher.php
@@ -8,7 +8,10 @@ class DownloadMatcher implements DownloadMatcherInterface
     /**
      * The url where ACF PRO can be downloaded (without version and key)
      */
-    private const ACF_PRO_PACKAGE_URL = 'https://connect.advancedcustomfields.com/index.php?p=pro&a=download';
+    private const ACF_PRO_PACKAGE_URL = 'https://connect.advancedcustomfields.com/v2/plugins/download?p=pro';
+
+
+    private const ACF_PRO_LEGACY_PACKAGE_URL = 'https://connect.advancedcustomfields.com/index.php?p=pro&a=download';
 
     /**
      * Returns if this download matches an ACF Package URL
@@ -18,6 +21,6 @@ class DownloadMatcher implements DownloadMatcherInterface
      */
     public function matches(string $url): bool
     {
-        return strpos($url, self::ACF_PRO_PACKAGE_URL) !== false;
+        return strpos($url, self::ACF_PRO_LEGACY_PACKAGE_URL) !== false || strpos($url, self::ACF_PRO_PACKAGE_URL) !== false;
     }
 }
